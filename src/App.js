df34fda9d10b513252666be6f9fa459e4c98b1d7
kimjunha <tto9011@naver.com>
2023-11-28 17:30:02 +0900

    dsa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{display:"flex",  alignItems:"center", flexDirection:"column"}}>
+        <h1>자기소개</h1>
+        <div>이름: 김준하</div>
+        <div>나이: 23</div>
+        <div>상태: 졸림</div>
+      </div>
+  
   );
 }
 
