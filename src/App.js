bd789862874a98c5fd4af421a9e00c1877869b32
kimjunha <tto9011@naver.com>
2023-12-03 14:46:49 +0900

    das

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div style={{display:"flex",  alignItems:"center", flexDirection:"column"}}>
         <h1>자기소개</h1>
         <div>이름: 김준하</div>
-        <div>나이: 23</div>
+        <div>나이:23</div>
         <div>상태: 졸림</div>
       </div>
   
